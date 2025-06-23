7b6cb6495b3e2fe17884b0b2eb00a1a38f3c83f2
ibrahiim82 <yavuzyolcu82@gmail.com>
2025-06-23 20:20:11 +0300

    up

--- src/redux/actions/auth.js
+++ src/redux/actions/auth.js
@@ -3,7 +3,7 @@ import { toast } from 'react-toastify'
 
 export const registerAction = (authData) => async(dispatch) => {
     try {
-        const {data} = await axios.post('https://stackprojectbe.onrender.com/register', authData)
+        const {data} = await axios.post(`${process.env.REACT_APP_API_URL}/register`, authData)
 
         dispatch({type:'REGISTER', payload: data})
 
@@ -18,7 +18,7 @@ export const registerAction = (authData) => async(dispatch) => {
 
 export const loginAction = (authData) => async(dispatch) => {
     try {
-        const {data} = await axios.post('https://stackprojectbe.onrender.com/login', authData)
+        const {data} = await axios.post(`${process.env.REACT_APP_API_URL}/login`, authData)
 
         dispatch({type:'LOGIN', payload: data})
 
